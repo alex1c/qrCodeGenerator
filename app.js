d358f8f6be4ede7f0e5f8d50b1a349dcc125b935
alex1c <alex1c.spb@gmail.com>
2021-11-28 22:47:17 +0300

    final

--- app.js
+++ app.js
@@ -8,41 +8,92 @@ const colors = [
   "d35400",
   "27ae60",
 ];
-const colorBlock = document.querySelector(".color-picker");
+//const colorBlock = document.querySelector(".color-picker");
+const colorBlock = document.querySelector(".my-flex-cont");
+let currentColor = "#0000CD";
 
 colors.forEach(function (color) {
   itemColor = document.createElement("div");
   itemColor.classList.add("dquare");
+  itemColor.classList.add('my-flex-box')
   itemColor.style.backgroundColor = "#" + color;
-  itemColor.innerHTML = "==========";
+  itemColor.style.color = "rgba(255,228,181)"
+  itemColor.innerHTML = "#" +color;
+  itemColor.addEventListener("click", (event) => {
+    console.log(event.target.style.backgroundColor);
+    currentColor = event.target.style.backgroundColor;
+    event.preventDefault();
+    document.getElementById("qrcode").innerHTML = "";
+    makeCode();
+  });
+
   colorBlock.append(itemColor);
 });
+
+const sizeBlock = document.querySelector(".custom-dropdown-option");
+sizeBlock.addEventListener("click", (event) => {
+  //console.log(event.target.style.backgroundColor);
+  // currentColor = event.target.style.backgroundColor
+  event.preventDefault();
+  document.getElementById("qrcode").innerHTML = "";
+  makeCode();
+});
+
+const inputTextArea = document.querySelector('#text')
+inputTextArea.addEventListener("blur", (event) => {
+    //console.log(event.target.style.backgroundColor);
+    // currentColor = event.target.style.backgroundColor
+    event.preventDefault();
+    document.getElementById("qrcode").innerHTML = "";
+    makeCode();
+  });
+
+  //const inputTextArea = document.querySelector('#text')
+inputTextArea.addEventListener("keydown", (event) => {
+    //console.log(event.target.style.backgroundColor);
+    // currentColor = event.target.style.backgroundColor
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        document.getElementById("qrcode").innerHTML = "";
+        makeCode();
+    }
+   
+  });
 
 /*
  * Original version at
  * http://red-team-design.com/making-html-dropdowns-not-suck
  */
+function getCurrentWidth() {
+  const width = document.querySelector(".custom-dropdown-option");
 
-const width = document.querySelector(".custom-dropdown-option");
-if ((width.value = "100x100")) {
-  widthValue = 100;
-} else if ((width.value = "128x128")) {
-  widthValue = 128;
-} else if ((width.value = "256x256")) {
-  widthValue = 256;
-} else if ((width.value = "512x512")) {
-  widthValue = 512;
-} else if ((width.value = "1024x1024")) {
-  widthValue = 1024;
+  console.log(width.options.selectedIndex);
+
+  if (width.options.selectedIndex == 0) {
+    widthValue = 100;
+    return widthValue;
+  } else if (width.options.selectedIndex == 1) {
+    widthValue = 128;
+    return widthValue;
+  } else if (width.options.selectedIndex == 2) {
+    widthValue = 256;
+    return widthValue;
+  } else if (width.options.selectedIndex == 3) {
+    widthValue = 512;
+    return widthValue;
+  } else if (width.options.selectedIndex == 4) {
+    widthValue = 1024;
+    return widthValue;
+  }
 }
 
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-  width: widthValue,
-  height: widthValue,
-  colorDark: "#FF4500",
-  colorLight: "#ffffff",
-  correctLevel: QRCode.CorrectLevel.H,
-});
+// var qrcode = new QRCode(document.getElementById("qrcode"), {
+//   width: widthValue,
+//   height: widthValue,
+//   colorDark: "#FF4500",
+//   colorLight: "#ffffff",
+//   correctLevel: QRCode.CorrectLevel.H,
+// });
 
 function makeCode() {
   var elText = document.getElementById("text");
@@ -52,18 +103,27 @@ function makeCode() {
     elText.focus();
     return;
   }
-  
+
+  var qrcode = new QRCode(document.getElementById("qrcode"), {
+    width: getCurrentWidth(),
+    height: widthValue,
+    colorDark: currentColor, //"#FF4500",
+    colorLight: "#ffffff",
+    text: "rrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+
   qrcode.makeCode(elText.value);
 }
 
 makeCode();
 
-$("#text")
-  .on("blur", function () {
-    makeCode();
-  })
-  .on("keydown", function (e) {
-    if (e.keyCode == 13) {
-      makeCode();
-    }
-  });
+// $("#text")
+//   .on("blur", function () {
+//     makeCode();
+//   })
+//   .on("keydown", function (e) {
+//     if (e.keyCode == 13) {
+//       makeCode();
+//     }
+//   });
